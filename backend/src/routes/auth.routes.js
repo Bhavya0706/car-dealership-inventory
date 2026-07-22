bc4d17100b4a33ebbac150b9b0e779c5d6b26541
Bhavya0706 <sutharbhavy77@gmail.com>
2026-07-22 23:23:27 +0530

    feat: add JWT authentication middleware
    taken a help of chatgpt

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const {registerValidator , loginValidator} = require('../validators/auth.validator');
 const {register , login } = require('../controllers/auth.controller');
+const { authenticate } = require("../middleware/auth.middleware"); 
 
 const router = express.Router();
 
 router.post("/register",registerValidator,register);
 router.post("/login" , loginValidator, login);
+router.get("/me", authenticate, (req, res) => {return res.status(200).json({
+        message: "route are protected",
+        user: req.user
+    });
+});
 
 module.exports = router;
